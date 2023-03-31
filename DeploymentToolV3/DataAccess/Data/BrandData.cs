ba9f28f22011fb5e70620d0561df4f3992673c70
siddugadag <siddu.gadag@gmail.com>
2023-03-31 22:58:52 +0530

    Brand API is completed

---
+++ DeploymentToolV3/DataAccess/Data/BrandData.cs
@@ -0,0 +1,89 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DataAccess.Models.BrandModel;
+
+namespace DataAccess.Data
+{
+    public class BrandData : IBrandData
+    {
+        private readonly ISQLDataAccess _db;
+        public BrandData(ISQLDataAccess db)
+        {
+            _db = db;
+        }
+        public async Task<IEnumerable<BrandModel>> GetBrands(BrandModel brand)
+        {
+            return await _db.LoadData<BrandModel, dynamic>(stroredProcedure: "", new { });
+        }
+
+        public async Task<BrandModel> GetBrandById(int BrandId, string BrandName, int UserID)
+        {
+            var inputParams = new
+            {
+                nBrandID = BrandId,
+                tBrandName = BrandName,
+                nUserID = UserID,
+                nPageSize = 10,
+                nPageNumber = 1
+            };
+            var result = await _db.LoadDataWithLists<BrandModel>("sprocBrandGet", inputParams);
+            BrandModel model = result.Item1;
+            return model;
+        }
+        public async Task<BrandModel> InsertBrand(BrandModel brand)
+        {
+            var inputParams = new
+            {
+                tBrandName = brand.tBrandName,
+                tBrandDescription = brand.tBrandDescription,
+                tBrandWebsite = brand.tBrandWebsite,
+                tBrandCountry = brand.tBrandCountry,
+                tBrandEstablished = brand.tBrandEstablished,
+                tBrandCategory = brand.tBrandCategory,
+                tBrandContact = brand.tBrandContact,
+                nBrandLogoAttachmentID = brand.nBrandLogoAttachmentID,
+                nUserID = brand.nUserID
+            };
+            var outputParameters = new { nBrandID = 0 };
+            var result = await _db.SaveData("sprocBrandCreate", inputParams, outputParameters);
+            if (result.TryGetValue("nBrandID", out object value))
+            {
+                brand.tBrandId = value != null && int.TryParse(value.ToString(), out int val) ? val : 0;
+            }
+
+            return brand;
+
+        }
+        public async Task<BrandModel> UpdateBrand(BrandModel brand)
+        {
+            var inputParams = new
+            {
+                nBrandId = brand.tBrandId,
+                tBrandName = brand.tBrandName,
+                tBrandDescription = brand.tBrandDescription,
+                tBrandWebsite = brand.tBrandWebsite,
+                tBrandCountry = brand.tBrandCountry,
+                tBrandEstablished = brand.tBrandEstablished,
+                tBrandCategory = brand.tBrandCategory,
+                tBrandContact = brand.tBrandContact,
+                nBrandLogoAttachmentID = brand.nBrandLogoAttachmentID,
+                nUserId = 1
+
+            };
+            var outputParameters = new { isSuccess = 0 };
+            var result = await _db.SaveData("sprocBrandUpdate", inputParams, outputParameters);
+            return brand;
+
+        }
+        public async Task DeleteBrand(int BrandID)
+        {
+
+            await _db.SaveData("SprocBrandDelete", new { nBrandId = BrandID, nUserID = 1 }, new { isSuccess = 0 });
+        }
+    }
+}
---
+++ DeploymentToolV3/WebApi/API/BrandAPI.cs
@@ -0,0 +1,65 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace WebApi.API
+{
+    public static class BrandAPI
+    {
+        public static void ConfigureBrandController(this WebApplication app)
+        {
+            app.MapGet(pattern: "/Brands", GetBrandById);
+            app.MapPost(pattern: "/Brands", InsertBrand);
+            app.MapPut(pattern: "/Brands", UpdateBrand);
+            app.MapDelete(pattern: "/Brands", DeleteBrand);
+        }
+        private static async Task<IResult> GetBrandById(int BrandId, string BrandName, int UserID, IBrandData data)
+        {
+            try
+            {
+                return Results.Ok(await data.GetBrandById(BrandId,BrandName,UserID));
+
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+        private static async Task<IResult> InsertBrand(BrandModel brand, IBrandData data)
+        {
+            try
+            {
+                return Results.Ok(await data.InsertBrand(brand));
+
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+        private static async Task<IResult> UpdateBrand(BrandModel brand, IBrandData data)
+        {
+            try
+            {
+                return Results.Ok(await data.UpdateBrand(brand));
+
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        private static async Task<IResult> DeleteBrand(int BrandId, IBrandData data)
+        {
+            try
+            {
+                await data.DeleteBrand(BrandId);
+                return Results.Ok();
+
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+    }
+}
